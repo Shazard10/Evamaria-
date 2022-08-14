2cdcd36d80aac465eeb3bcc20a8438fd13694224
Shanid TG <109721076+Shazard10@users.noreply.github.com>
2022-08-14 10:13:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -710,7 +710,7 @@ async def auto_filter(client, msg, spoll=False):
         BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
-            [InlineKeyboardButton(text=f"【 1-{math.ceil(int(total_results) / 10)} 】", callback_data="pages"),
+            [InlineKeyboardButton(text=f"【 1-{math.ceil(int(total_results) - 10)} 】", callback_data="pages"),
              InlineKeyboardButton(text="ɴᴇxᴛ➪", callback_data=f"next_{req}_{key}_{offset}")]
         )
         btn.append(
